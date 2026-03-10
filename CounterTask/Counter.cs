1aba156a1a6c1143ac4b7e2639677f4842260f57
Shaun Vambe <you@example.com>
2026-03-10 16:31:52 +1100

    New

---
+++ CounterTask/Counter.cs
@@ -0,0 +1,53 @@
+
+using System;
+
+public class Counter
+{
+    private int _count;
+    private string _name;
+
+    public Counter(string name)
+    {
+        _name = name;
+        _count = 0;
+    }
+
+    public void Increment()
+    {
+        _count++;
+    }
+
+    public void Reset()
+    {
+        _count = 0;
+    }
+
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+    public int Ticks
+    {
+        get
+        {
+            return _count;
+        }
+    }
+
+    public void ResetbyDefault()
+    {
+        _count = 214748350;
+    }
+    public void incrementbyfive ()
+        {
+            _count = _count + 5;
+        }
+    }
